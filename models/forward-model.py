1bb14a5cc578fced1861f2467a7a54fc4dc5e158
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-04-02 14:27:15 +0100

    Testing the forward model with ONE09 data

--- models/forward-model.py
+++ models/forward-model.py
@@ -7,9 +7,24 @@ Petit et al. (2017)"""
 # Import libraries
 import numpy as np
 import math
+import pandas as pd
 
 # Access the absorption, backscattering, and benthic reflectance data
 
+total_a = pd.read_csv('C:/Users/pirtapalola/Documents/iop_data/data/total_a.csv')
+total_a = total_a.rename({'ONE09_r_rs': 'ONE09_a'}, axis='columns')
+ONE09_a = list(total_a['ONE09_a'])
+print(ONE09_a)
+
+total_bb = pd.read_csv('C:/Users/pirtapalola/Documents/iop_data/data/total_bb.csv')
+total_bb = total_bb.rename({'ONE09_Kd': 'ONE09_bb'}, axis='columns')
+ONE09_bb = list(total_bb['ONE09_bb'])
+print(ONE09_bb)
+
+rrs_benthic = pd.read_csv('C:/Users/pirtapalola/Documents/iop_data/data/benthic_reflectance.csv')
+rrs_benthic = rrs_benthic.rename({'ONE09_MGT': 'ONE09_rrs_benthic'}, axis='columns')
+ONE09_rrs_benthic = list(rrs_benthic['ONE09_rrs_benthic'])
+print(ONE09_rrs_benthic)
 
 # Sub-surface solar zenith angle in radians
 # Refractive index of seawater (temperature = 20C, salinity = 25g/kg, light wavelength = 589.3 nm)
@@ -31,30 +46,39 @@ theta_w = math.asin(inv_refractive_index * math.sin(math.radians(theta_air)))
 
 # Lee et al. (1999) Eqs.(6)
 
-kappa = a + bb
-u_var = bb/kappa
+kappa = [ONE09_a[x] + ONE09_bb[x] for x in range(len(ONE09_a))]
+u_var = [ONE09_bb[x]/kappa[x] for x in range(len(ONE09_bb))]
 
 # Lee et al. (1999) Eqs. (5)
 # Optical path-elongation factors for scattered photons
 
-du_water = 1.03 * np.power((1.00 + (2.40 * u_var)), 0.50)  # from the water column
-du_bottom = 1.04 * np.power((1.00 + (5.40 * u_var)), 0.50)  # from the bottom
+du_water = [1.03 * np.power((1.00 + (2.40 * n)), 0.50) for n in u_var]  # from the water column
+du_bottom = [1.04 * np.power((1.00 + (5.40 * n)), 0.50) for n in u_var]  # from the bottom
 
 # Lee et al. (1999) Eq. (4)
 # Remotely sensed sub-surface reflectance for optically deep water
 
-rrsdp = (0.084 + 0.17 * u_var) * u_var
+rrsdp = [(0.084 + 0.17 * n) * n for n in u_var]
 
 # Define variables
 
 inv_cos_theta_w = 1.0 / math.cos(theta_w)
 depth = 0.9
-kappa_d = kappa * depth
+kappa_d = [n * depth for n in kappa]
 
 # Lee et al. (1999) Eq. (3)
 # Remote sensing reflectance
 # Assumption: nadir-viewing sensor
 
-rrs = (rrsdp * (1.0 - np.exp(-(inv_cos_theta_w + du_water) * kappa_d)) +
-       ((1.0 / math.pi) * r_substratum *
-        np.exp(-(inv_cos_theta_w + du_bottom) * kappa_d)))
+rrs_ONE09 = [(rrsdp[x] * (1.0 - np.exp(-(inv_cos_theta_w + du_water[x]) * kappa_d[x])) +
+              ((1.0 / math.pi) * ONE09_rrs_benthic[x] *
+               np.exp(-(inv_cos_theta_w + du_bottom[x]) * kappa_d[x]))) for x in range(len(rrsdp))]
+
+# Save the results in a pandas dataframe
+
+rrs_data = pd.DataFrame()
+rrs_data['ONE09'] = rrs_ONE09
+print(rrs_data)
+
+# Save the dataframe to a csv
+rrs_data.to_csv('C:/Users/pirtapalola/Documents/iop_data/data/forward_model_results.csv')
