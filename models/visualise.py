cc785a1f35e0a9165bb44a80c86bea397460667b
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-04-12 20:32:35 +0100

    Minor edits

--- models/visualise.py
+++ models/visualise.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
 
-os.chdir(r'C:/Users/pirtapalola/Documents/iop_data/data/trios')
+os.chdir(r'C:/Users/pirtapalola/Documents/iop_data/data/M94_data')
 the_list = []
-trios_list = []
+m94_list = []
 
 # Create a list that contains the paths of all the csv files in a folder
 for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/iop_data/data/forward_model'):
@@ -12,11 +12,11 @@ for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/iop_data/data/
             the_list.append(file)
 
 # Do the same for the folder with the trios data
-for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/iop_data/data/trios'):
+for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/iop_data/data/M94_data'):
     for file in files:
         if file.endswith('.csv'):
-            trios_list.append(file)
-print(trios_list)
+            m94_list.append(file)
+print(m94_list)
 # Define a function that takes a list of paths, reads all the csv files,
 # and adds the first column to a new pandas dataframe
 
@@ -25,11 +25,11 @@ def readdataframe(list_paths):
     df = pd.DataFrame()  # define df as an empty pandas DataFrame
     for element in list_paths:
         # print(element)
-        df[element] = pd.read_csv(element, sep=',', header=0, usecols=[2])
+        df[element] = pd.read_csv(element, sep=',', header=0, usecols=[1])
     return df
 
 
-#fm_reflectance = readdataframe(the_list)
-#fm_reflectance.to_csv('C:/Users/pirtapalola/Documents/iop_data/data/fm_reflectance.csv')
-trios_reflectance = readdataframe(trios_list)
-trios_reflectance.to_csv('C:/Users/pirtapalola/Documents/iop_data/data/trios_reflectance.csv')
+# fm_reflectance = readdataframe(the_list)
+# fm_reflectance.to_csv('C:/Users/pirtapalola/Documents/iop_data/data/fm_reflectance.csv')
+m94_reflectance = readdataframe(m94_list)
+m94_reflectance.to_csv('C:/Users/pirtapalola/Documents/iop_data/data/m94_reflectance.csv')
