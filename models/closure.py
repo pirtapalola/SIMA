1f59ea4c29025e5dc0fd54d0d1ac5c3acca6f0e7
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-04-14 10:52:22 +0100

    Code for SAM and SID

--- models/closure.py
+++ models/closure.py
@@ -1,52 +1,50 @@
+"""This code implements Spectral Angle Mapping (SAM) and Spectral Information Divergence (SID)
+    to quantify the similarity of the measured and modelled spectra.
+
+References (SAM):
+    Dennison et al. (2004)
+    Kruse et al. (1993)
+    Schwarz & Staenz (2001)
+
+SAM has been applied by, for example:
+    Brando et al. (2009)
+    Kutser et al. (2006)
+    Petit et al. (2017)
+
+References (SID):
+    Chang (2000)
+"""
+
+
+# Import libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import math
-from lmfit import Parameters, Minimizer
-from matplotlib import rcParams
+import pysptools.distance
 
+# Read the csv data into a pandas dataframe
+closure_data = pd.read_csv("C:/Users/pirtapalola/Documents/iop_data/data/closure_experiment.csv")
 
-def read_csv_data(filename):
-    dataset = pd.read_csv(filename)
-    return dataset
-
-
-closure_data = read_csv_data("C:/Users/pirtapalola/Documents/iop_data/data/closure_experiment.csv")
+# List containing the unique IDs of the sampling sites
 sites = ['ONE07', 'ONE08', 'ONE09', 'ONE10', 'ONE11', 'ONE12',
          'RIM01', 'RIM02', 'RIM03', 'RIM04', 'RIM05', 'RIM06']
-rrs = closure_data['ONE07_surface']
-plt.plot(rrs)
-# plt.show()
+benthic_sites = [x+'_benthic' for x in sites]
+surface_sites = [x+'_surface' for x in sites]
+lee99_sites = [x+'_L99' for x in sites]
+maritorena94_sites = [x+'_M94' for x in sites]
 
 
-def forward1(params, data=None):
-    phy = params['phy']
-    cdom = params['cdom']
-    nap = params['nap']
-    zb = params['zb']
-    theta_air = params['theta_air']
-
-    # Phytoplankton absorption coefficient
-
-    # Sub-surface solar zenith angle in radians
-    # Refractive index of seawater (temperature = 20C, salinity = 25g/kg, light wavelength = 589.3 nm)
-    water_refractive_index = 1.33784
-    inv_refractive_index = 1.0 / water_refractive_index
-    # Sub-surface viewing angle in radians
-    theta_w = math.asin(inv_refractive_index * math.sin(math.radians(theta_air)))
-    inv_cos_theta_w = 1.0 / math.cos(theta_w)
-    # Diffuse attenuation coefficient
-    kd_coefficient = inv_cos_theta_w
+def make_numpy_arrays(dataframe, column_name):
+    return np.array(dataframe[column_name])
 
 
+test = make_numpy_arrays(closure_data, benthic_sites[0])
+print(test)
+s0 = np.array(closure_data['ONE07_benthic'])
+s1 = np.array(closure_data['ONE07_surface'])
+s2 = np.array(closure_data['ONE07_L99'])
+s3 = np.array(closure_data['ONE07_M94'])
 
-
-    kappa = [aw[x] + bbw[x] for x in range(len(aw))]
-    u_var = [bbw[x] / kappa[x] for x in range(len(bbw))]
-    rw = [(0.084 + 0.17 * n) * n for n in u_var]
-    model = [rw[x] + (rb[x] - rw[x])
-             * np.exp(-2 * kd[x] * zb) for x in range(len(rb))]
-    if data is None:
-        return model
-    else:
-        return [model[x]-data[x] for x in range(len(data))]
+ONE07_L99_SAM = pysptools.distance.SAM(s1, s0)
+ONE07_M94_SAM = pysptools.distance.SAM(s1, s3)
+print(ONE07_L99_SAM)
+print(ONE07_M94_SAM)
