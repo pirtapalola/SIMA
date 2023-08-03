8d2efd0e9b31b676f94406ebbb7ccf677a9e5e26
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-08-03 10:14:38 +0100

    updated runlist after correction of path specification

--- models/runlistHL.py
+++ models/runlistHL.py
@@ -3,10 +3,10 @@
 import os
 
 # Access the input files
-os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_sbi_setup')
+os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_sbi_setup_v2')
 the_list = []
 
-for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_sbi_setup'):
+for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_sbi_setup_v2'):
     for file in files:
         if file.endswith('.txt'):
             the_list.append(file)
@@ -14,7 +14,7 @@ for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2
 
 def new_runlist_file(path_list):
     # open file in write mode
-    path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_final_setup/runlist.txt'
+    path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_final_setup/runlist_v2.txt'
     with open(path, 'w') as fp:
         for item in path_list:
             fp.write(item + ' \n')
