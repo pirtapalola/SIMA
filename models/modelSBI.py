71bfe5da782c869e1bac6b2b11559e5a6750717f
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-08-25 13:33:00 +0100

    Simplified the code

--- models/modelSBI.py
+++ models/modelSBI.py
@@ -3,13 +3,10 @@
 Simulation-based inference
 
 STEP 1. Prepare the simulated data.
-STEP 2. Split the data into training and validation sets.
-STEP 3. Define the amortized neural network architecture.
-STEP 4. Instantiate the amortized neural network.
-STEP 5. Train and validate the neural network.
-STEP 6. Evaluate the neural network with field-collected data.
+STEP 2.
 
-Last updated on 22 August 2023 by Pirta Palola
+
+Last updated on 25 August 2023 by Pirta Palola
 
 """
 
@@ -40,54 +37,39 @@ print("Current working directory:", current_dir)
 path = 'data/simulated_data/'  # Define the file location
 files = [f for f in os.listdir(path) if f.endswith('.txt')]  # Create a list of all the files in the folder
 
-
 # Define the simulated dataset
 num_simulation_runs = len(files)  # Number of reflectances simulated in HydroLight
 num_parameters = 5  # Chl-a, SPM, CDOM, wind speed, and depth
 num_output_values = 150  # Hyperspectral reflectance between 400nm and 700nm at 2nm spectral resolution
-
 
 # Read the csv file containing the simulated Rrs data into a pandas dataframe
 simulated_reflectance = pd.read_csv('data/HL_output_combined_dataframe.csv')
 simulated_reflectance.iloc[:, 0] = files  # Replace the first column repeating "Rrs" with the corresponding file names
 simulated_reflectance.rename(columns={simulated_reflectance.columns[0]: "File_ID"}, inplace=True)  # Rename the column
 
-# Apply the function
+# Apply the function to create a dataframe containing the inputs of each of the HydroLight simulation runs
 hydrolight_input = create_input_dataframe(files)
 print(hydrolight_input)
 
-
 # Print the minimum and maximum values of each column in the dataframe
+# These should correspond to the empirically realistic range of values.
 minimum_maximum(hydrolight_input, ["phy", "cdom", "spm", "wind", "depth"])
 
-# Define the input and output values for the neural network
-input_parameters = simulated_reflectance.drop(columns="File_ID")  # Drop the File_ID column
-output_values = hydrolight_input
+# Define theta and x
+theta_dataframe = hydrolight_input  # Theta contains the five input variables.
+x_dataframe = simulated_reflectance.drop(columns="File_ID")  # The output values are stored in x. Drop the File_ID.
 
+# Convert the pandas DataFrames to numpy arrays
+theta_array = theta_dataframe.to_numpy()
+x_array = x_dataframe.to_numpy()
 
-"""STEP 2. Split the data into training and validation datasets."""
-
-train_size = int(0.8 * num_simulation_runs)  # 80% for training
-train_input = input_parameters[:train_size]
-train_output = output_values[:train_size]
-
-val_input = input_parameters[train_size:]
-val_output = output_values[train_size:]
-
-# Convert the pandas DataFrame to a numpy array
-train_input_array = train_input.to_numpy()
-train_output_array = train_output.to_numpy()
-val_input_array = val_input.to_numpy()
-val_output_array = val_output.to_numpy()
-
-# Convert input parameters and training output to PyTorch tensors
-train_input_tensor = torch.tensor(train_input_array, dtype=torch.float32)
-train_output_tensor = torch.tensor(train_output_array, dtype=torch.float32)
-
+# Convert the numpy arrays to PyTorch tensors
+theta_tensor = torch.tensor(theta_array, dtype=torch.float32)
+x_tensor = torch.tensor(x_array, dtype=torch.float32)
 
 """
 
-STEP 6. Define the prior distributions.
+STEP 2. Define the prior.
 
 Each parameter is associated with its own distribution and name.
 
@@ -117,30 +99,43 @@ prior = MultipleIndependent(prior_distributions)
 # combined_train_data = torch.cat([train_input_tensor, train_output_tensor], dim=1)
 # print(train_input_tensor)
 # print(train_output_tensor)
-"""
-Arguments for append_simulations():
 
-1) A tensor containing the simulated data, structured as follows:
-        - Each row represents a single simulation run.
-        - The columns should contain both the input parameters and the corresponding observed (output) data.
-        - The order of the columns matters. The input parameters should come before the observed data.
-2) prior_log_probs: This argument is optional.
-        - If you provide prior log probabilities for your simulated data, 
-          you can pass them as a tensor of shape (num_simulations,).
-        - The log probabilities should correspond to the provided simulations.   
 """
 
+STEP 3. Instantiate the inference object and pass the simulated data to the inference object.
+
+"""
+
+# Instantiate the SNPE inference method
 inference = SNPE(prior=prior)
-# Append the combined data to the inference method
-inference.append_simulations(train_input_tensor, train_output_tensor)
 
-# Train the inference method
+# Append the combined data to the inference object
+inference.append_simulations(theta_tensor, x_tensor)
+
+"""
+
+STEP 4. Train the neural density estimator and build the posterior.
+
+"""
+
+# Train the neural density estimator
 density_estimator = inference.train()
+
+# Use the trained neural density estimator to build the posterior
 posterior = inference.build_posterior(density_estimator)
 
-x_o = train_output_tensor[1]
+# Define an observation x
+x_o = x_tensor[1]
+
+# Given this observation, sample from the posterior p(θ|x), or plot it.
 posterior_samples = posterior.sample((10000,), x=x_o)
 
-# plot posterior samples
-_ = analysis.pairplot(posterior_samples)
+# Evaluate the log-probability of the posterior samples
+log_probability = posterior.log_prob(posterior_samples, x=x_o)
+
+# Plot posterior samples
+_ = analysis.pairplot(posterior_samples, limits=[[0, 10], [0, 5], [0, 30], [0, 10], [0, 20]], figsize=(6, 6))
 plt.show()
+
+# Print the posterior to know how it was trained
+print(posterior)
