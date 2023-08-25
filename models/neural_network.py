9456ea79e815b25a9a4e6e5ad9bea48697a49fbe
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-08-25 14:18:05 +0100

    Corrected error expected output data dimension

--- models/neural_network.py
+++ models/neural_network.py
@@ -2,8 +2,8 @@
 
 Implementation of a neural network
 STEP 1. Read in the datasets.
-STEP 2. Define the amortized neural network architecture.
-STEP 3. Instantiate the amortized neural network.
+STEP 2. Define the neural network architecture.
+STEP 3. Instantiate the neural network.
 STEP 4. Split the data into training and validation datasets.
 STEP 5. Train and validate the neural network.
 
@@ -24,67 +24,63 @@ import torch.optim as optim
 STEP 1. Read in the datasets.
 
 """
-# Create a list of all the filenames
+
+# Create a list of all the filenames in the folder
 path = 'data/simulated_data/'  # Define the file location
 files = [f for f in os.listdir(path) if f.endswith('.txt')]  # Create a list of all the files in the folder
-
-
-# Define the simulated dataset
 num_simulation_runs = len(files)  # Number of reflectances simulated in HydroLight
-num_parameters = 5  # Chl-a, SPM, CDOM, wind speed, and depth
-num_input_values = 150  # Hyperspectral reflectance between 400nm and 700nm at 2nm spectral resolution
-
 
 # Read the csv file containing the simulated Rrs data into a pandas dataframe
 simulated_reflectance = pd.read_csv('data/HL_output_combined_dataframe.csv')
 simulated_reflectance.iloc[:, 0] = files  # Replace the first column repeating "Rrs" with the corresponding file names
 simulated_reflectance.rename(columns={simulated_reflectance.columns[0]: "File_ID"}, inplace=True)  # Rename the column
 
-# Apply the function to create the input dataframe based on the information in the filenames
-hydrolight_input = create_input_dataframe(files)
-print(hydrolight_input)
+# Apply the function to create the output dataframe based on the information in the filenames
+output_dataframe = create_input_dataframe(files)
+print(output_dataframe)
 
 
 """
 
-STEP 2. Define the amortized neural network architecture.
+STEP 2. Define the neural network architecture.
 
 """
 
 
-class AmortizedPosterior(nn.Module):
+class SimpleNeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
-        super(AmortizedPosterior, self).__init__()
+        super(SimpleNeuralNetwork, self).__init__()
 
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
 
-        self.network_layers = nn.Sequential(
+        self.network = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, output_dim * 2)  # Two parameters per inferred parameter
+            nn.Linear(hidden_dim, output_dim)
         )
 
     def forward(self, input_data):
-        posterior_params = self.network_layers(input_data)
-        return posterior_params
+        output = self.network(input_data)
+        return output
 
 
 """
 
-STEP 3. Instantiate the amortized neural network.
+STEP 3. Instantiate the neural network.
 
 """
 
-# Define key variables
-input_dim = num_input_values
-output_dim = num_parameters
+# Define the input and output dimensions
+input_dim = 150  # Hyperspectral reflectance between 400nm and 700nm at 2nm spectral resolution
+output_dim = 5  # Chl-a, SPM, CDOM, wind speed, and depth
+
+# Instantiate the neural network
 hidden_dim = 256
-amortized_net = AmortizedPosterior(input_dim, output_dim, hidden_dim)
-
+neural_net = SimpleNeuralNetwork(input_dim, hidden_dim, output_dim)
 
 """
 
@@ -93,16 +89,16 @@ STEP 4. Split the data into training and validation datasets.
 """
 
 # Define input and output
-input_parameters = simulated_reflectance.drop(columns="File_ID")  # Drop the File_ID column
-output_values = hydrolight_input
+input_values = simulated_reflectance.drop(columns="File_ID")  # Drop the File_ID column
+output_values = output_dataframe
 
 # Training datasets
 train_size = int(0.8 * num_simulation_runs)  # 80% for training
-train_input = input_parameters[:train_size]
+train_input = input_values[:train_size]
 train_output = output_values[:train_size]
 
 # Validation datasets
-val_input = input_parameters[train_size:]
+val_input = input_values[train_size:]
 val_output = output_values[train_size:]
 
 # Convert the pandas DataFrame to a numpy array
@@ -111,9 +107,11 @@ train_output_array = train_output.to_numpy()
 val_input_array = val_input.to_numpy()
 val_output_array = val_output.to_numpy()
 
-# Convert input parameters and training output to PyTorch tensors
+# Convert numpy arrays to PyTorch tensors
 train_input_tensor = torch.tensor(train_input_array, dtype=torch.float32)
 train_output_tensor = torch.tensor(train_output_array, dtype=torch.float32)
+val_input_tensor = torch.tensor(val_input_array, dtype=torch.float32)
+val_output_tensor = torch.tensor(val_output_array, dtype=torch.float32)
 
 """
 
@@ -123,7 +121,7 @@ STEP 5. Train and validate the neural network.
 
 # Define loss function and optimizer
 criterion = nn.MSELoss()  # Mean squared error loss
-optimizer = optim.Adam(amortized_net.parameters(), lr=0.001)
+optimizer = optim.Adam(neural_net.parameters(), lr=0.001)
 
 # Lists to store loss values for plotting
 train_losses = []
@@ -136,7 +134,7 @@ for epoch in range(num_epochs):
     optimizer.zero_grad()  # Reset gradients
 
     # Forward pass
-    train_predictions = amortized_net(train_input_tensor)
+    train_predictions = neural_net(train_input_tensor)
 
     # Compute loss
     loss = criterion(train_predictions, train_output_tensor)
@@ -149,8 +147,8 @@ for epoch in range(num_epochs):
 
     # Validation
     with torch.no_grad():
-        val_predictions = amortized_net(torch.tensor(val_input_array, dtype=torch.float32))
-        val_loss = criterion(val_predictions, torch.tensor(val_output_array, dtype=torch.float32))
+        val_predictions = neural_net(val_input_tensor)
+        val_loss = criterion(val_predictions, val_output_tensor)
     # Store loss values for plotting
     train_losses.append(loss.item())
     val_losses.append(val_loss.item())
