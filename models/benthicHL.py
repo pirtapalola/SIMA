dbf161ad327f2c53643fb29aa41fbe9cbabaf26f
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-11-19 15:50:29 +0000

    Corrected a typo in a comment

--- models/benthicHL.py
+++ models/benthicHL.py
@@ -7,7 +7,7 @@ wavelength = [str(i) for i in wavelength_data]
 reflectance = [str(i) for i in reflectance_data]
 
 # open file in write mode
-path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/HL_benthic/avg_coral.txt'
+path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/avg_coral.txt'
 
 
 # hydrolight_file[65] = r'D:\HE53\data\User\microplastics\MPzdata.txt' + '\n'
@@ -18,15 +18,37 @@ def write_wavelength_file(wavelength_list):
             fp.write(' ' + item + '   ' + '\n')
 
 
-write_wavelength_file(wavelength)
+# write_wavelength_file(wavelength)
 
+# Create empty lists to store data
+lines = []
+data_list = []
 
-with open(path) as fp:
-    lines = fp.read().splitlines()
-with open(path, "w") as fp:
-    for line in lines:
-        for value in reflectance:
-            print(line + value, file=fp)
+# Read the txt file
+with open(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/avg_coral.txt', 'r') as fp:
+    lines = fp.readlines()  # Add each line of the txt file as an element in the list
 
+# Delete the first 6 elements and the last element of the list
+data = lines[5:]
+data = data[:-1]
 
-print(tetiaroa_data)
+# Split each string in the list into two separate strings and store as sublists in a list
+for i in range(len(data)):
+    data_list.append(data[i].split("   "))
+
+# Create separate lists for the Hydrolight benthic reflectance data
+wavelength_HL = []
+reflectance_HL = []
+
+for x in range(len(data)):
+    wavelength_HL.append(data_list[x][0].replace(' ', ''))  # Remove empty spaces
+
+for x in range(len(data_list)):
+    reflectance_HL.append(data_list[x][1].replace('\n', ''))  # Remove extra characters
+
+# Add the Hydrolight benthic reflectance data to a pandas dataframe
+hl_reflectance_df = pd.DataFrame({"wavelength": wavelength_HL, "avg_coral": reflectance_HL})
+print(hl_reflectance_df)
+
+# Save the data into a csv file.
+hl_reflectance_df.to_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/avg_coral_HL.csv')
