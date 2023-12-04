4b6ae853acea313b7b3dd1523dd01a2776aaa249
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-12-04 18:53:50 +0000

    calculate average reflectances

---
+++ models/hochberg.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Read the csv file containing the simulated Rrs data into a pandas dataframe
+global_df = pd.read_csv('C:/Users/pirtapalola/Documents/DPhil/'
+                        'Chapter2/Methods/global_benthic_reflectance_hochberg.csv')
+
+# Remove the dot and the number from each column name
+global_df.columns = global_df.columns.str.replace(r'\.\d+$', '', regex=True)
+
+# Calculate row average for columns with the same name
+global_df_mean = global_df.groupby(global_df.columns, axis=1).mean()
+print(global_df_mean.keys())
+
+# Save to a csv file
+global_df_mean.to_csv('C:/Users/pirtapalola/Documents/DPhil/'
+                      'Chapter2/Methods/global_benthic_reflectance_hochberg_mean.csv')
