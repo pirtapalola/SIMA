66ad07118777ebc429b6ac0978367430f4bced37
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-12-08 10:50:55 +0000

    Minor edits

--- models/plotting.py
+++ models/plotting.py
@@ -9,8 +9,8 @@ def prior_lognormal(loc, scale):
 
 
 # Generate samples from the lognormal distribution using PyTorch
-prior_wind = torch.from_numpy(lognorm.rvs(s=0.13295081858033553, loc=-9.08382565107291,
-                                             scale=15.666340917893361, size=5000)).float()
+# prior_lognormal_chl = torch.distributions.log_normal.LogNormal(torch.tensor([0.4]), torch.tensor([1.6]))
+prior_chl = torch.from_numpy(lognorm.rvs(s=0.4, loc=0, scale=1.6, size=5000)).float()
 
 # Generate the prior distributions
 prior_chl = prior_lognormal(0.4, 1.6)
@@ -19,8 +19,8 @@ prior_spm = prior_lognormal(1.3, 1.1)
 #prior_wind = prior_lognormal(2.75, 0.13)
 
 # Plot the histogram of the prior distribution
-plt.hist(prior_wind.numpy(), bins=5000, density=True, color='blue', alpha=0.7)
-plt.xlabel('Wind speed')
+plt.hist(prior_chl.numpy(), bins=5000, density=True, color='blue', alpha=0.7)
+plt.xlabel('Wind speed (m/s)')
 plt.ylabel('Density')
-plt.xlim(0, 50)
+plt.xlim(0, 20)
 plt.show()
