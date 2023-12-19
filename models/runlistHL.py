b4c448e8ba2c0d90f0e2593996cdce81bd836b8d
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2023-12-19 17:44:04 +0000

    update filepaths

--- models/runlistHL.py
+++ models/runlistHL.py
@@ -5,7 +5,7 @@ Create the runlist.txt file for the Hydrolight simulations.
 STEP 1. Save the input file names into a list.
 STEP 2. Write the runlist file containing all the file names.
 
-Last updated on 05 December 2023 by Pirta Palola
+Last updated on 19 December 2023 by Pirta Palola
 
 """
 
@@ -15,10 +15,10 @@ import os
 
 """"STEP 1. Save the input file names into a list."""
 
-os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/test_setup')
+os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/setup')
 the_list = []
 
-for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/test_setup'):
+for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/setup'):
     for file in files:
         if file.endswith('.txt'):
             the_list.append(file)
@@ -29,7 +29,7 @@ for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2
 
 def new_runlist_file(path_list):
     # open file in write mode
-    path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/runlist_test_051223.txt'
+    path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/runlist.txt'
     with open(path, 'w') as fp:
         for item in path_list:
             fp.write(item + ' \n')
