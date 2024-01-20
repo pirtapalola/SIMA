d7becadd048c0d7096fc43922fe66e6adcf3c6ef
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-01-20 17:24:03 +0000

    save calibrated values in a csv file

--- models/field_data/radiometer_calibration.py
+++ models/field_data/radiometer_calibration.py
@@ -30,29 +30,46 @@ measurement = pd.read_csv("C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods
 
 # List of sample IDs
 sample_IDs = list(measurement.columns)
+sample_IDs = sample_IDs[1::]  # Delete the first element of the list
 print(sample_IDs)
+
+# List of irradiance sample IDs
+e_sample_IDs = sample_IDs[0::2]
+print(len(e_sample_IDs))
+
+# List of radiance sample IDs
+l_sample_IDs = sample_IDs[1::2]
+print(len(l_sample_IDs))
 
 "STEP 2. Calibrate the reflectance data."
 
 
-def calibrate_reflectance(air878A, aq878A, air8789, aq8789, e_data, l_data):
+def calibrate_reflectance(air878a, aq878a, air8789, aq8789, e_data, l_data):
     # Multiply the in-air calibrated data with the in-air calibration factor to get back to the raw values.
-    e_raw = [e_data[i] * air878A[i] for i in range(len(e_data))]
-    l_raw = [l_data[i] * air8789[i] for i in range(len(e_data))]
+    e_raw = [e_data[item] * air878a[item] for item in range(len(e_data))]
+    l_raw = [l_data[item] * air8789[item] for item in range(len(e_data))]
     # Divide the raw values with the in-water calibration factor.
-    e_cal = [e_raw[i] / aq878A[i] for i in range(len(e_data))]
-    l_cal = [l_raw[i] / aq8789[i] for i in range(len(e_data))]
+    e_cal = [e_raw[item] / aq878a[item] for item in range(len(e_data))]
+    l_cal = [l_raw[item] / aq8789[item] for item in range(len(e_data))]
     # Calculate reflectance (radiance divided by irradiance).
-    reflectance = l_cal/e_cal
+    reflectance = [l_cal[item] / e_cal[item] for item in range(len(e_cal))]
     return reflectance
 
 
+# Create an empty list to store the data
+refl_list = []
+
+# Apply the function to all the samples using a loop
+for x in range(len(e_sample_IDs)):
+    refl = calibrate_reflectance(air_878A_2022, aq_878A_2022, air_8789_2022, aq_8789_2022,
+                                 measurement[e_sample_IDs[x]], measurement[l_sample_IDs[x]])
+    refl_list.append(refl)  # Append the results into a list
+
 "STEP 3. Save the calibrated values into a csv file."
 
-cal_values = pd.DataFrame()
-cal_values["one02_e1_cal"] = rim01_e1_cal
-cal_values["one02_l1_cal"] = rim01_l1_cal
-cal_values["one02_rrs"] = [rim01_l1_cal[i] / rim01_e1_cal[i] for i in range(len(rim01_e1_cal))]
-
+cal_values = pd.DataFrame(columns=e_sample_IDs)
+for i in refl_list:
+    for x in e_sample_IDs:
+        cal_values[x] = i
 cal_values.to_csv("C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/"
-                  "Methods_Ecolight/In_water_calibration_2022/ONE02_2022_calibrated.csv")
+                  "Methods_Ecolight/In_water_calibration_2022/calibrated_surface_measurements_tetiaroa_2022.csv")
