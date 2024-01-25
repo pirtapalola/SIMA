ec429dd0bffedef1387ca65d8da217cda7f07042
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-01-25 12:32:19 +0000

    create prior summary file

--- models/merge_to_a_single_file.py
+++ models/merge_to_a_single_file.py
@@ -2,7 +2,7 @@
 
 Merge data from multiple files into a single csv file.
 
-Last updated on 21 January 2024 by Pirta Palola
+Last updated on 25 January 2024 by Pirta Palola
 
 """
 
@@ -12,14 +12,14 @@ import pandas as pd
 
 # Define the folder
 os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-         r'Dec2023_lognormal_priors/results/theta_intervals')
+         r'Jan2024_lognormal_priors/priors')
 
 # Create an empty list
 the_list = []
 
 # Create a list that contains the paths of all the csv files in a folder
 for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-                                 r'Dec2023_lognormal_priors/results/theta_intervals'):
+                                 r'Jan2024_lognormal_priors/priors'):
     for file in files:
         if file.endswith('.csv'):
             the_list.append(file)
@@ -40,4 +40,4 @@ the_dataframe = readdataframe(the_list)
 
 # Save the dataframe as a csv file
 the_dataframe.to_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-                     'Dec2023_lognormal_priors/results/theta_intervals_summary.csv')
+                     'Jan2024_lognormal_priors/priors/summary_priors.csv')
