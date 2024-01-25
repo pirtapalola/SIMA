073da5768f14b2398aec7c8fa135997e08f7fcd2
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-01-25 11:10:41 +0000

    new runlist.txt file

--- models/simulation_setup/runlistHL.py
+++ models/simulation_setup/runlistHL.py
@@ -15,10 +15,11 @@ import os
 
 """"STEP 1. Save the input file names into a list."""
 
-os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/setup')
+os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/check_simulation/setup')
 the_list = []
 
-for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/setup'):
+for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
+                                 r'check_simulation/setup'):
     for file in files:
         if file.endswith('.txt'):
             the_list.append(file)
@@ -29,7 +30,7 @@ for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2
 
 def new_runlist_file(path_list):
     # open file in write mode
-    path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/runlist.txt'
+    path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/check_simulation/runlist.txt'
     with open(path, 'w') as fp:
         for item in path_list:
             fp.write(item + ' \n')
