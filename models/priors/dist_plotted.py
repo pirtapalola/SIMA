a5233e7dc78ffca08f1801ea5a0e0425c15a1b48
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-02-05 15:02:38 +0000

    plot PDFs with the new priors

--- models/priors/dist_plotted.py
+++ models/priors/dist_plotted.py
@@ -1,10 +1,10 @@
 """
 
 Visualise the prior distributions:
- -Plot the 3000 samples drawn from each prior distribution to a histogram.
- -Add the corresponding prior distribution as a curve.
+ -Plot the 10,000 samples drawn from each prior distribution to a histogram.
+ -Add the corresponding PDF as a curve.
 
-Last updated on 19 December 2023 by Pirta Palola
+Last updated on 5 February 2024 by Pirta Palola
 
 """
 
@@ -12,19 +12,30 @@ Last updated on 19 December 2023 by Pirta Palola
 import torch
 import pandas as pd
 import matplotlib.pyplot as plt
+from models.tools import TruncatedLogNormal
+import numpy as np
 
-# Define the priors
-prior1 = torch.distributions.LogNormal(0.1, 1.7)
-prior2 = torch.distributions.LogNormal(0.05, 1.7)
-prior3 = torch.distributions.LogNormal(0.4, 1.1)
+# Create an instance of TruncatedLogNormal
+prior1 = TruncatedLogNormal(loc=0, scale=5, upper_bound=5)
+prior2 = TruncatedLogNormal(loc=0, scale=5, upper_bound=2)
+prior3 = TruncatedLogNormal(loc=0, scale=5, upper_bound=20)
 prior4 = torch.distributions.LogNormal(1.85, 0.33)
+
+# Plot the PDF for visualization
+x_values1 = np.linspace(0.01, 5, 1000)
+x_values2 = np.linspace(0.01, 2, 1000)
+x_values3 = np.linspace(0.01, 20, 1000)
+
+pdf_values1 = prior1.pdf(torch.tensor(x_values1))
+pdf_values2 = prior2.pdf(torch.tensor(x_values2))
+pdf_values3 = prior3.pdf(torch.tensor(x_values3))
 
 # Import the samples drawn from each prior (3000 randomly drawn samples)
 prior_samples = pd.read_csv("C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/"
-                            "Methods_Ecolight/Dec2023_lognormal_priors/priors/priors_summary.csv")
+                            "Methods_Ecolight/Jan2024_lognormal_priors/priors/priors_summary.csv")
 samples_phy = prior_samples["Phytoplankton"].to_numpy()
 samples_cdom = prior_samples["CDOM"].to_numpy()
-samples_nap = prior_samples["NAP"].to_numpy()
+samples_nap = prior_samples["SPM"].to_numpy()
 samples_wind = prior_samples["Wind"].to_numpy()
 
 
@@ -32,41 +43,32 @@ samples_wind = prior_samples["Wind"].to_numpy()
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
 
 # Plot the PDF and histogram of prior1 in the first subplot
-x = torch.linspace(prior1.icdf(torch.tensor(0.00001)).item(),
-                   prior1.icdf(torch.tensor(0.999)).item(), 5000)
-p = prior1.log_prob(x).exp().numpy()
-axes[0, 0].plot(x, p, '#184e77', linewidth=2, label='$\mu=0.1$, $\sigma=1.7$')
-axes[0, 0].hist(samples_phy, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 3000)')
+axes[0, 0].plot(x_values1, pdf_values1, '#184e77', linewidth=2, label='$\mu=0$, $\sigma=5$')
+axes[0, 0].hist(samples_phy, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 10,000)')
 axes[0, 0].legend()
 axes[0, 0].set_title('Phytoplankton')
-axes[0, 0].set_xlim(left=0, right=10)  # Set x-axis limits
-axes[0, 0].set_ylim(bottom=0, top=1.0)  # Set y-axis limits
+axes[0, 0].set_xlim(left=0, right=5)  # Set x-axis limits
+axes[0, 0].set_ylim(bottom=0, top=5.0)  # Set y-axis limits
 axes[0, 0].set_xlabel('Concentration (mg/$\mathregular{m^3}$)')
 axes[0, 0].set_ylabel('Probability Density')
 
 # Plot the PDF and histogram of prior2 in the second subplot
-x = torch.linspace(prior2.icdf(torch.tensor(0.00001)).item(),
-                   prior2.icdf(torch.tensor(0.999)).item(), 5000)
-p = prior2.log_prob(x).exp().numpy()
-axes[0, 1].plot(x, p, '#184e77', linewidth=2, label='$\mu=0.05$, $\sigma=1.7$')
-axes[0, 1].hist(samples_cdom, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 3000)')
+axes[0, 1].plot(x_values2, pdf_values2, '#184e77', linewidth=2, label='$\mu=0$, $\sigma=5$')
+axes[0, 1].hist(samples_cdom, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 10,000)')
 axes[0, 1].legend()
 axes[0, 1].set_title('CDOM')
-axes[0, 1].set_xlim(left=0, right=5)  # Set x-axis limits
-axes[0, 1].set_ylim(bottom=0, top=1.0)  # Set y-axis limits
+axes[0, 1].set_xlim(left=0, right=2)  # Set x-axis limits
+axes[0, 1].set_ylim(bottom=0, top=5.0)  # Set y-axis limits
 axes[0, 1].set_xlabel('Absorption $\mathregular{m^-1}$ at 440 nm)')
 axes[0, 1].set_ylabel('Probability Density')
 
 # Plot the PDF and histogram of prior3 in the third subplot
-x = torch.linspace(prior3.icdf(torch.tensor(0.00001)).item(),
-                   prior3.icdf(torch.tensor(0.999)).item(), 5000)
-p = prior3.log_prob(x).exp().numpy()
-axes[1, 0].plot(x, p, '#184e77', linewidth=2, label='$\mu=0.4$, $\sigma=1.1$')
-axes[1, 0].hist(samples_nap, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 3000)')
+axes[1, 0].plot(x_values3, pdf_values3, '#184e77', linewidth=2, label='$\mu=0$, $\sigma=5$')
+axes[1, 0].hist(samples_nap, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 10,000)')
 axes[1, 0].legend()
-axes[1, 0].set_title('NAP')
-axes[1, 0].set_xlim(left=0, right=50)  # Set x-axis limits
-axes[1, 0].set_ylim(bottom=0, top=1.0)  # Set y-axis limits
+axes[1, 0].set_title('SPM')
+axes[1, 0].set_xlim(left=0, right=20)  # Set x-axis limits
+axes[1, 0].set_ylim(bottom=0, top=5.0)  # Set y-axis limits
 axes[1, 0].set_xlabel('Concentration (g/$\mathregular{m^3}$)')
 axes[1, 0].set_ylabel('Probability Density')
 
@@ -75,11 +77,11 @@ x = torch.linspace(prior4.icdf(torch.tensor(0.00001)).item(),
                    prior4.icdf(torch.tensor(0.999)).item(), 5000)
 p = prior4.log_prob(x).exp().numpy()
 axes[1, 1].plot(x, p, '#184e77', linewidth=2, label='$\mu=1.85$, $\sigma=0.33$')
-axes[1, 1].hist(samples_wind, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 3000)')
+axes[1, 1].hist(samples_wind, bins=1000, alpha=0.5, color='#76c893', density=True, label='Samples (n = 10,000)')
 axes[1, 1].legend()
 axes[1, 1].set_title('Wind')
 axes[1, 1].set_xlim(left=0, right=20)  # Set x-axis limits
-axes[1, 1].set_ylim(bottom=0, top=1.0)  # Set y-axis limits
+axes[1, 1].set_ylim(bottom=0, top=5.0)  # Set y-axis limits
 axes[1, 1].set_xlabel('Wind speed (m/s)')
 axes[1, 1].set_ylabel('Probability Density')
 
