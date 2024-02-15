5b0a1f632b9dda5bbc83d05f86403878ec8c28b5
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-02-15 20:27:22 +0000

    change path

--- models/test.py
+++ models/test.py
@@ -2,14 +2,13 @@
 import pandas as pd
 from io import StringIO
 import os
-from multiprocessing import Pool
 
-os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/check_simulation_Feb2024/el_setup3')
+os.chdir(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/check_simulation_Feb2024/new_test')
 the_list = []
 
 # Create a list that contains the paths of all the csv files in a folder
 for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/'
-                                 r'Methods_Ecolight/check_simulation_Feb2024/el_setup3'):
+                                 r'Methods_Ecolight/check_simulation_Feb2024/new_test'):
     for file in files:
         if file.endswith('.csv'):
             the_list.append(file)
@@ -19,14 +18,14 @@ for root, dirs, files in os.walk(r'C:/Users/pirtapalola/Documents/DPhil/Chapter2
 def process_file(file_path):
     with open(file_path, 'r') as file:
         text = file.read()
-        lines = text.strip().split('\n')[629:780]  # Extract lines 630-780
+        lines = text.strip().split('\n')[269:329]  # Extract lines
         data = "\n".join(lines)  # Join the lines and create a StringIO object
         data_io = StringIO(data)
         df = pd.read_csv(data_io, sep=r'\s+', header=None)  # Read the data into a pandas DataFrame
         df = df.T  # Transpose the DataFrame to get the desired format
         df.columns = df.iloc[0]  # Set the first row as the header
         df = df.iloc[:3]
-        df = df.drop([0, 2])
+        #df = df.drop([0, 2])
     return df
 
 
@@ -40,4 +39,4 @@ dataframe = pd.DataFrame(empty_list)
 print(dataframe)
 
 dataframe.to_csv("C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/"
-                 "Methods_Ecolight/check_simulation_Feb2024/simulated_reflectance_test_Feb2024.csv")
+                 "Methods_Ecolight/check_simulation_Feb2024/new_test.csv")
