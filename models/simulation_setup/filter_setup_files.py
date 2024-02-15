be203cdd022149d392cf14f6d31a207352fbd639
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-02-15 19:42:15 +0000

    filter and modify set-up files

--- models/simulation_setup/filter_setup_files.py
+++ models/simulation_setup/filter_setup_files.py
@@ -3,10 +3,9 @@
 Filter the set-up files to retrieve those that have NAP > 10 g/m3.
 STEP 1. Read the parameter combinations from a csv file.
 STEP 2. Store each row in the csv file as a tuple in a list.
-STEP 3. Create the file ID associated with each tuple.
-STEP 4. Add the file IDs as a column in the dataframe.
-STEP 5. Use the list of file IDs to create a list of filepaths
-        so that only the files that meet the condition NAP > 10 g/m3 are included.
+STEP 3. Filter the list so that only the tuples that meet the condition NAP >= 10 g/m3 are included.
+STEP 4. Create the file IDs associated with the selected tuples.
+STEP 5. Use the list of file IDs to create a list of filepaths.
 STEP 6. Modify the selected files.
 
 """
@@ -15,9 +14,6 @@ STEP 6. Modify the selected files.
 import pandas as pd
 import csv
 import glob
-from io import StringIO
-import os
-from multiprocessing import Pool
 
 """STEP 1. Read the parameter combinations from a csv file."""
 
@@ -39,16 +35,19 @@ with open(csv_file_path, 'r', newline='') as file:
         data_list.append(row_tuple)
 
 # Remove the first element of the list (a tuple containing the column names)
-print(data_list[1])
 data_list = data_list[1:]
 
-# Drop the last element from each tuple
-modified_data_list = [tuple(row[:-1]) for row in data_list]
-
 # Convert each item in each tuple into a float
-float_data_list = [tuple(map(float, row)) for row in modified_data_list]
+float_data_list = [tuple(map(float, row)) for row in data_list]
+print(len(float_data_list))  # This corresponds to the number of simulation runs
 
-"""STEP 3. Create the file ID associated with each tuple."""
+"""STEP 3. Filter the list so that only the tuples that meet the condition [NAP] >= 10 g/m3 are included."""
+
+# Create a new list that only contains the tuples that meet the condition.
+filtered_list = [tup for tup in float_data_list if tup[3] >= 10]
+print(len(filtered_list))  # This is the number of files that meet the condition
+
+"""STEP 4. Create the file IDs associated with the selected tuples."""
 
 
 # Use the same function that was used for naming the EL output files in the "inference" script.
@@ -61,11 +60,45 @@ def convert_tuple(tup):
     return new_string
 
 
+# Apply the function to the filtered data list
 string_id = []
-for i in float_data_list:
+for i in filtered_list:
     string_id.append(convert_tuple(i))
 
-"""STEP 4. Add the file IDs as a column in the dataframe."""
+"""STEP 5. Use the list of file IDs to create a list of filepaths."""
 
-combinations_df = combinations
-combinations_df["filename"] = string_id
+# Specify path to the folder containing the set-up files
+folder_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
+              'Jan2024_lognormal_priors/setup'
+file_ids = string_id
+
+# Create an empty list to store the filepaths
+file_paths = []
+
+# Iterate through file IDs and match corresponding files in the folder
+for file_id in file_ids:
+    pattern = f"{folder_path}/*{file_id}*"
+    matching_files = glob.glob(pattern)
+
+    if matching_files:
+        # Assuming there is only one matching file for each file ID
+        file_paths.append(matching_files[0])
+
+# Now, file_paths contains the paths to the files specified by file_ids.
+
+"""STEP 6. Modify the selected files."""
+
+
+def modify_setup_files(setup_file_path):
+    with open(setup_file_path, 'r') as setup_file:
+        lines = setup_file.readlines()
+        # Update the selected lines
+        lines[14] = r'..\data\defaults\astarmin_average.txt' + '\n'
+        lines[22] = r'..\data\defaults\bstarmin_average.txt' + '\n'
+    # Write the updated content back to the file
+    with open(setup_file_path, 'w') as setup_file:
+        setup_file.writelines(lines)
+
+
+for selected_file in file_paths:
+    modify_setup_files(selected_file)
