1c76cd27b922976b32da996a8739fbc68602c3e9
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-02-15 11:34:14 +0000

    test different filters

--- models/theta_similarity.py
+++ models/theta_similarity.py
@@ -12,7 +12,7 @@ from scipy.spatial.distance import euclidean
 path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
        'Jan2024_lognormal_priors/'
 theta_df = pd.read_csv(path + "Ecolight_parameter_combinations.csv")
-theta_df = theta_df.drop(['water', 'wind'], axis=1)
+theta_df = theta_df.drop(['water', 'wind', 'phy', 'cdom', 'depth'], axis=1)
 reference_theta = [0.1, 3.0, 0.1, 1.1]
 
 # Calculate Euclidean distance between reference row and all rows in the DataFrame
@@ -25,10 +25,11 @@ distance_df = pd.DataFrame(distance_scores, columns=['Distance'], index=theta_df
 top_10_rows = distance_df.sort_values(by='Distance').head(10)
 
 # Specify the range for each column
-column_ranges = {'phy': (0.05, 0.3),
-                 'cdom': (0.05, 0.5),
-                 'spm': (2, 4),
-                 'depth': (0.5, 2.0)}
+column_ranges = {#'phy': (2.0, 7.0)
+                 #'cdom': (1.0, 2.5)
+                 'spm': (10, 30)
+                 #'depth': (0.5, 2.0)
+}
 
 # Iterate over rows and check if values are within the specified range for each column
 selected_rows = []
