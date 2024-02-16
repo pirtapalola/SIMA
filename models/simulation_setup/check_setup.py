f530541e7d15ee32c88c4a7d72f02dddb046d3c0
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-02-16 11:49:15 +0000

    change filenames

--- models/simulation_setup/check_setup.py
+++ models/simulation_setup/check_setup.py
@@ -2,9 +2,9 @@
 
 # reading files
 f1 = open('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-          'Jan2024_lognormal_priors/setup/Icorals_00_001_009_1413_944_1002_coralbrown.txt', 'r')
+          'Jan2024_lognormal_priors/setup/Icorals_00_04_131_2274_415_1927_coralbrown.txt', 'r')
 f2 = open('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-          'Jan2024_lognormal_priors/setup/Icorals_00_001_009_42_744_1001_coralbrown.txt', 'r')
+          'Jan2024_lognormal_priors/setup/Icorals_00_248_151_007_422_1348_coralbrown.txt', 'r')
 
 f1_data = f1.readlines()
 f2_data = f2.readlines()
