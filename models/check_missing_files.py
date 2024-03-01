548cc25dafb7f9163912a73fdefbf062603e36e3
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-03-01 15:10:25 +0000

    create a new runlist file and copy selected setup files to a new folder

--- models/check_missing_files.py
+++ models/check_missing_files.py
@@ -1,65 +1,144 @@
+"""
+
+Check the simulation output and find missing files.
+STEP 1. Specify paths.
+STEP 2. Create a list of file IDs corresponding to the parameter combinations.
+STEP 3. Check for matching files.
+STEP 4. Create a new runlist.txt file for the missing simulations.
+STEP 5. Copy the missing set-up files into a new folder.
+
+Last updated on 1 March 2024 by Pirta Palola
+
+"""
+
 
 # Import libraries
+import pandas as pd
+import csv
 import os
+import shutil
 
-folder_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
-              'Methods_Ecolight/Jan2024_lognormal_priors/simulated_dataset/simulated_dataset'
+"""STEP 1. Specify paths."""
+
+csv_file_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
+                'Methods_Ecolight/Jan2024_lognormal_priors/Ecolight_parameter_combinations.csv'
+data_folder_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
+              'Methods_Ecolight/Jan2024_lognormal_priors/simulated_dataset/simulated_dataset/'
 file_runlist_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
                     'Methods_Ecolight/Jan2024_lognormal_priors/runlist_all.txt'
 output_file_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
                    'Methods_Ecolight/Jan2024_lognormal_priors/runlist.txt'
+all_setup_folder_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
+                   'Methods_Ecolight/Jan2024_lognormal_priors/setup_all_files/'
+
+"""STEP 2. Create a list of file IDs corresponding to the parameter combinations."""
+
+combinations = pd.read_csv(csv_file_path)
+data_list = []
+
+with open(csv_file_path, 'r', newline='') as file:
+    csv_reader = csv.reader(file)
+
+    for row in csv_reader:
+        # Convert each row to a tuple and append it to the list
+        row_tuple = tuple(row)
+        data_list.append(row_tuple)
+
+# Remove the first element of the list (a tuple containing the column names)
+data_list = data_list[1:]
+
+# Drop the last element from each tuple
+modified_data_list = [tuple(row[:-1]) for row in data_list]
+
+# Convert each item in each tuple into a float
+float_data_list = [tuple(map(float, row)) for row in modified_data_list]
 
 
-# Specify the number of characters to remove from the start and end
-start_slice = 14
-end_slice = 22
+# Use the same function that was used for naming the EL output files in the "inference" script.
+def convert_tuple(tup):
+    empty_string = ''
+    for item in tup:
+        element = round(item, 3)
+        empty_string = empty_string + '_' + str(element)
+        new_string = empty_string.replace('.', '')
+    return new_string
 
-# List to store sliced strings
-sliced_strings = []
 
-# Read the text file and slice each string
+string_id = []
+for i in float_data_list:
+    string_id.append(convert_tuple(i))
+
+print("Number of parameter combinations: ", len(string_id))
+
+"""STEP 3. Check for matching files."""
+
+
+def get_file_names(folder_path):
+    # Get the list of filenames in the specified folder
+    file_names = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    return file_names
+
+
+# Get the list of file names in the folder
+file_list = get_file_names(data_folder_path)
+
+# Matching files
+matches = [string1 for string1 in file_list for string2 in string_id if string2 in string1]
+
+# Print the number of matches
+print(f"Number of matches: {len(matches)}")
+
+# Using set to store unique values from list2 with no match in list1
+non_match_values = set(string2 for string2 in string_id if not any(string2 in file_name for file_name in file_list))
+
+# Print the unique values with no match
+print("Unique values in list2 with no match in list1:", len(non_match_values))
+
+"""STEP 4. Create a new runlist.txt file for the missing simulations."""
+
+# Open the file and read lines
 with open(file_runlist_path, 'r') as file:
-    for line in file:
-        # Remove specified number of characters from the start and end
-        sliced_string = line[start_slice:-end_slice].strip()
-        sliced_strings.append(sliced_string)
+    lines = file.readlines()
 
-print(sliced_strings[0])
+# Strip newline characters from each line and store in a list
+lines = [line.strip() for line in lines]
+print("Number of lines in the runlist_all.txt file: ", len(lines))
+
+# Create a set of filenames from lines that match non_match_values
+non_match_values_runlist = {file_name for file_name in lines if
+                            any(string2 in file_name for string2 in non_match_values)}
+
+# Convert the set to a list if needed
+non_match_values_runlist = list(non_match_values_runlist)
+
+# Print the list of filenames
+print(len(non_match_values_runlist))
+
+"""
+# Write non_match_values_runlist to a new text file
+with open(output_file_path, 'w') as output_file:
+    for item in non_match_values_runlist:
+        output_file.write(item + '')
+"""
+
+"""STEP 5. Copy the missing set-up files into a new folder."""
 
 
-# Lists to store files
-matching_files = []  # Files containing sliced strings
-non_matching_files = []  # Files not containing sliced strings
+def copy_selected_files(source_folder, destination_folder, selected_files):
+    # Ensure the destination folder exists, create it if necessary
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
-# Iterate through files in the folder
-for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
+    for file_name in selected_files:
+        source_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(destination_folder, file_name)
 
-    # Check if the file is a regular file
-    if os.path.isfile(file_path):
-        # Check if any sliced string is present in the filename
-        if any(sliced_string in filename for sliced_string in sliced_strings):
-            matching_files.append(file_path)
-        else:
-            non_matching_files.append(file_path)
+        # Copy the file
+        shutil.copy2(source_path, destination_path)
 
 
-# Print
-print("Number of matching files: ", len(matching_files))
-print("Number of non-matching files: ", len(non_matching_files))
+new_folder = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/' \
+                     'Methods_Ecolight/Jan2024_lognormal_priors/missing_files/'
 
-# Read the content of the text file
-with open(file_runlist_path, 'r') as text_file:
-    lines = text_file.readlines()
-
-# Identify lines to be removed
-lines_to_remove = [line for line in lines for sliced_string in sliced_strings if sliced_string in line]
-print("Lines to remove: ", lines_to_remove)
-
-# Remove identified lines
-updated_lines = [line for line in lines if line not in lines_to_remove]
-print("Updated lines: ", updated_lines)
-
-# Write the updated content to the new text file
-with open(output_file_path, 'w') as new_text_file:
-    new_text_file.writelines(updated_lines)
+# Copy selected files from source to destination folder
+copy_selected_files(all_setup_folder_path, new_folder, non_match_values_runlist)
