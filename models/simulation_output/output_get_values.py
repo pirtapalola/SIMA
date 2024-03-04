d0d60afdc39a22d8fb0dec220bb1701ad0a70127
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-03-04 19:24:28 +0000

    create a list of file paths

--- models/simulation_output/output_get_values.py
+++ models/simulation_output/output_get_values.py
@@ -9,7 +9,7 @@ STEP 5. Use the list of file IDs to create a list of filepaths
         so that each file can be accessed in the order defined by the list of file IDs.
 STEP 6. Extract reflectance from the EL output files.
 
-Last updated on 1 March 2024 by Pirta Palola
+Last updated on 4 March 2024 by Pirta Palola
 
 """
 
@@ -17,14 +17,12 @@ Last updated on 1 March 2024 by Pirta Palola
 import pandas as pd
 import csv
 import glob
-from io import StringIO
-import os
-from multiprocessing import Pool
+
 
 """STEP 1. Read the parameter combinations from a csv file."""
 
 csv_file_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
-                'check_simulation_Feb2024/Ecolight_parameter_combinations.csv'
+                'Jan2024_lognormal_priors/Ecolight_parameter_combinations.csv'
 combinations = pd.read_csv(csv_file_path)
 
 
@@ -76,7 +74,7 @@ so that each file can be accessed in the order defined by the list of file IDs."
 
 # Specify path to the folder containing the output files
 folder_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
-              'check_simulation_Feb2024/el_setup3'
+              'Jan2024_lognormal_priors/simulated_dataset/simulated_dataset'
 file_ids = combinations_df["filename"]
 
 # Create an empty list to store the filepaths
@@ -92,47 +90,7 @@ for file_id in file_ids:
         file_paths.append(matching_files[0])
 
 # Now, file_paths contains the paths to the files in the order specified by file_ids.
-
-"""STEP 6. Extract reflectance from the EL output files."""
-
-
-# A function to process a single file
-def process_file(file_path):
-    with open(file_path, 'r') as file:
-        text = file.read()
-        lines = text.strip().split('\n')[629:780]  # Extract lines 630-780
-        data = "\n".join(lines)  # Join the lines and create a StringIO object
-        data_io = StringIO(data)
-        df = pd.read_csv(data_io, sep=r'\s+', header=None)  # Read the data into a pandas DataFrame
-        df = df.T  # Transpose the DataFrame to get the desired format
-        df.columns = df.iloc[0]  # Set the first row as the header
-        df = df.iloc[:3]
-        df = df.drop([0, 2])
-    return df
-
-
-# Apply the function to all the files
-def main():
-    output_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/check_simulation_Feb2024/' \
-                  'simulated_rrs_check_simulation.csv'
-
-    # Number of processes to use (adjust as needed)
-    num_processes = 4
-
-    # Create a Pool of processes
-    with Pool(num_processes) as pool:
-        data_frames = pool.map(process_file, file_paths)
-
-    # Concatenate DataFrames
-    combined_df = pd.concat(data_frames, ignore_index=True)
-
-    # Save the combined DataFrame to a CSV file
-    combined_df.to_csv(output_path, index=False)
-
-
-# This condition checks if the script is being run directly as the main program
-# (not imported as a module into another script).
-# This ensures that the code within the main() function only runs when the script is executed directly.
-
-if __name__ == '__main__':  # Check if the script is being run directly
-    main()  # If so, call the main() function to start executing the script
+# Save the file_paths into a csv file.
+df = pd.DataFrame(file_paths)
+df.to_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
+          'Jan2024_lognormal_priors/file_paths_output_processing.csv')
