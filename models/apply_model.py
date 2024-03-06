35386e3a13b58513b5e95ce5a19119ea50142900
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-03-06 16:00:47 +0000

    add constant and log-transformation

--- models/apply_model.py
+++ models/apply_model.py
@@ -5,7 +5,7 @@ STEP 1. Load the posterior and the simulated reflectance data.
 STEP 2. Load the observation data.
 STEP 3. Infer the parameters corresponding to the observation data.
 
-Last updated on 5 March 2024 by Pirta Palola
+Last updated on 6 March 2024 by Pirta Palola
 
 """
 
@@ -39,21 +39,32 @@ obs_df = pd.read_csv(observation_path + 'field_surface_reflectance_brown_coral.c
 # Read the file containing the corresponding parameters
 obs_parameters = pd.read_csv(observation_path + 'parameters_brown_coral.csv')
 
+# Add a constant to avoid issues with the log-transformation of small values
+constant = 1.0
+samples_phy = [i+constant for i in obs_parameters["phy"]]
+samples_cdom = [i+constant for i in obs_parameters["cdom"]]
+samples_nap = [i+constant for i in obs_parameters["spm"]]
+samples_wind = obs_parameters["wind"]
+samples_depth = obs_parameters["depth"]
+
+# Conduct the log-transformation
+samples_phy = np.log(samples_phy)
+samples_cdom = np.log(samples_cdom)
+samples_nap = np.log(samples_nap)
+samples_wind = np.log(samples_wind)
+
+# Save the transformed data in a dataframe
+transformed_dictionary = {"phy": samples_phy, "cdom": samples_cdom, "spm": samples_nap,
+                          "wind": samples_wind, "depth": samples_depth}
+
+transformed_theta = pd.DataFrame(data=transformed_dictionary)
+
 # Create a list of sample IDs
-sample_IDs = list(obs_df.columns)
+sample_IDs = obs_parameters["unique_ID"]
 print(sample_IDs)
 
-# Log-transform the parameter values
-for sample in sample_IDs:
-    obs_parameters[sample] = np.log(obs_parameters[sample])
-
-
-# Test simulation run
-# x_o_test = x_dataframe.iloc[10]
-# posterior_samples_test = loaded_posterior.sample((1000,), x=x_o_test)  # Sample from the posterior p(θ|x)
-# print(x_o_test)
-
 """STEP 3. Infer the parameters corresponding to the observation data."""
+
 results_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
                'Jan2024_lognormal_priors/results/'
 
@@ -67,18 +78,17 @@ def infer_from_observation(sample_id):
     log_probability = loaded_posterior.log_prob(posterior_samples, x=x_obs)
     log_prob_np = log_probability.numpy()  # Convert to Numpy array
     log_prob_df = pd.DataFrame(log_prob_np)  # Convert to a dataframe
-    log_prob_df.to_csv(results_path + 'log_probability/' + sample_id + '_log_probability.csv')
+    log_prob_df.to_csv(results_path + sample_id + '_log_probability.csv')
     theta_samples = posterior_samples.numpy()  # Convert to NumPy array
     # Mean estimates for each parameter
     theta_means = torch.mean(posterior_samples, dim=0)
     theta_means_df = pd.DataFrame(theta_means)  # Convert to a dataframe
-    theta_means_df.to_csv(results_path + 'theta_means/' + sample_id + '_theta_means.csv')
+    theta_means_df.to_csv(results_path + sample_id + '_theta_means.csv')
     # Credible intervals (e.g., 95% interval) for each parameter using NumPy
     theta_intervals = np.percentile(theta_samples, [2.5, 97.5], axis=0)
     theta_intervals_df = pd.DataFrame(theta_intervals)  # Convert to a dataframe
-    theta_intervals_df.to_csv(results_path + 'theta_intervals/' + sample_id + '_theta_intervals.csv')
+    theta_intervals_df.to_csv(results_path + sample_id + '_theta_intervals.csv')
     # Create the figure
-    # _ = analysis.pairplot(posterior_samples, limits=[[0, 10], [0, 5], [0, 30], [0, 10], [0, 20]], figsize=(6, 6))
     _ = analysis.pairplot(
         samples=posterior_samples,
         points=x_obs_parameters,
@@ -90,9 +100,9 @@ def infer_from_observation(sample_id):
         scatter_offdiag=dict(marker=".", s=5),
         points_offdiag=dict(marker="+", markersize=20)
     )
-    plt.savefig(results_path + 'figures/' + sample_id + '.png')
+    plt.savefig(results_path + sample_id + '.png')
 
 
 # Apply the function to real observations
-# for item in sample_IDs:
-#    infer_from_observation(item)
+for item in sample_IDs:
+    infer_from_observation(item)
