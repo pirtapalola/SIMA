cdf1bf0f0529676541bf5985c397272f2c9f2c7e
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-03-07 11:26:11 +0000

    increase number of posterior samples

--- models/check_0.py
+++ models/check_0.py
@@ -15,20 +15,21 @@ import pickle
 """STEP 1."""
 
 # Read the csv file containing the simulated reflectance data into a pandas dataframe
-simulated_reflectance = pd.read_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/'
+simulated_reflectance = pd.read_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/' 
                                     'Methods/Methods_Ecolight/Jan2024_lognormal_priors/checks/check_0/check0_x.csv')
 # Read the csv file containing the inputs of each of the HydroLight simulation runs
 ecolight_input = pd.read_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/'
                              'Methods/Methods_Ecolight/Jan2024_lognormal_priors/checks/check_0/check0_theta.csv')
 
 # Define theta and x.
-theta_example = ecolight_input.iloc[5]  # Theta contains the five input variables
+theta_example = ecolight_input.iloc[32]  # Theta contains the five input variables
+print(theta_example)
 constant = 1.0  # Add a constant to avoid issues with the log-transformation of small values
 theta_example[:3] += constant  # Only add the constant to the first 3 theta parameters
 for x in range(4):  # Apply the log-transformation to the first 4 theta parameters
     theta_example[x] = np.log(theta_example[x])
 
-x_array = simulated_reflectance.iloc[5]  # X contains the simulated spectra
+x_array = simulated_reflectance.iloc[32]  # X contains the simulated spectra
 
 # Convert to tensors
 theta_tensor = torch.tensor(theta_example, dtype=torch.float32)
@@ -46,12 +47,12 @@ results_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ec
 
 
 def infer_from_simulated_spectra(x_sim, x_sim_parameters):
-    posterior_samples = loaded_posterior.sample((1000,), x=x_sim)  # Sample from the posterior p(θ|x)
+    posterior_samples = loaded_posterior.sample((10000,), x=x_sim)  # Sample from the posterior p(θ|x)
     # Create the figure
     _ = pairplot(
         samples=posterior_samples,
         points=x_sim_parameters,
-        limits=[[0, 1], [0, 1], [0, 5], [0, 5], [0, 20]],
+        limits=[[0, 5], [0, 5], [0, 10], [0, 5], [0, 20]],
         points_colors=["red", "red", "red", "red", "red"],
         figsize=(8, 8),
         labels=["Phytoplankon", "CDOM", "NAP", "Wind speed", "Depth"],
