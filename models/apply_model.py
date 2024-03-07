9aeb7678bc7f2b2d8d754603b2599002b858c30f
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-03-07 09:51:19 +0000

    input theta and x as tensors

--- models/apply_model.py
+++ models/apply_model.py
@@ -35,6 +35,7 @@ x_dataframe = simulated_reflectance.drop(columns={simulated_reflectance.columns[
 observation_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
                    'Jan2024_lognormal_priors/field_data/'
 obs_df = pd.read_csv(observation_path + 'field_surface_reflectance_brown_coral.csv')
+print(obs_df)
 
 # Read the file containing the corresponding parameters
 obs_parameters = pd.read_csv(observation_path + 'parameters_brown_coral.csv')
@@ -71,30 +72,43 @@ results_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ec
 
 
 def infer_from_observation(sample_id):
-    x_obs = obs_df[sample_id]
-    x_obs_parameters = transformed_theta.loc[transformed_theta['unique_ID'] == sample_id]
-    # x_o = min_max_normalisation(x_obs)  # Apply max-min normalisation
-    posterior_samples = loaded_posterior.sample((1000,), x=x_obs)  # Sample from the posterior p(θ|x)
+
+    # Define x
+    x_obs = obs_df[sample_id].to_list()
+    x_obs = torch.tensor(x_obs, dtype=torch.float32)
+
+    # Sample from the posterior p(θ|x)
+    posterior_samples = loaded_posterior.sample((1000,), x=x_obs)
+
+    # Define theta
+    theta_obs = transformed_theta.loc[transformed_theta['unique_ID'] == sample_id]
+    theta_obs = theta_obs.drop(columns="unique_ID")
+    theta_obs = theta_obs.iloc[0].to_list()
+    theta_obs = torch.tensor(theta_obs, dtype=torch.float32)
+
     # Evaluate the log-probability of the posterior samples
     log_probability = loaded_posterior.log_prob(posterior_samples, x=x_obs)
     log_prob_np = log_probability.numpy()  # Convert to Numpy array
     log_prob_df = pd.DataFrame(log_prob_np)  # Convert to a dataframe
     log_prob_df.to_csv(results_path + sample_id + '_log_probability.csv')
     theta_samples = posterior_samples.numpy()  # Convert to NumPy array
+
     # Mean estimates for each parameter
     theta_means = torch.mean(posterior_samples, dim=0)
     theta_means_df = pd.DataFrame(theta_means)  # Convert to a dataframe
     theta_means_df.to_csv(results_path + sample_id + '_theta_means.csv')
+
     # Credible intervals (e.g., 95% interval) for each parameter using NumPy
     theta_intervals = np.percentile(theta_samples, [2.5, 97.5], axis=0)
     theta_intervals_df = pd.DataFrame(theta_intervals)  # Convert to a dataframe
     theta_intervals_df.to_csv(results_path + sample_id + '_theta_intervals.csv')
+
     # Create the figure
     _ = analysis.pairplot(
         samples=posterior_samples,
-        points=x_obs_parameters,
-        limits=[[0, 1], [0, 1], [0, 10], [0, 10], [0, 2]],
-        points_colors=["red"],
+        points=theta_obs,
+        limits=[[0, 1], [0, 1], [0, 5], [0, 10], [0, 2]],
+        points_colors=["red", "red", "red", "red", "red"],
         figsize=(8, 8),
         labels=["Phytoplankon", "CDOM", "NAP", "Wind speed", "Depth"],
         offdiag="scatter",
@@ -105,5 +119,5 @@ def infer_from_observation(sample_id):
 
 
 # Apply the function to real observations
-for item in sample_IDs:
-    infer_from_observation(item)
+for i in sample_IDs:
+    infer_from_observation(i)
