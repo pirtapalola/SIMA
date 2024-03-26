c3db4f272021e0e923618c241c6958f8ca39cb12
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-03-26 17:40:54 +0000

    correct indexing and new posterior

--- models/check_0.py
+++ models/check_0.py
@@ -1,7 +1,7 @@
 """
 Conduct inference on simulated data.
 
-Last updated on 6 March 2024 by Pirta Palola
+Last updated on 26 March 2024 by Pirta Palola
 """
 
 # Import libraries
@@ -16,20 +16,27 @@ import pickle
 
 # Read the csv file containing the simulated reflectance data into a pandas dataframe
 simulated_reflectance = pd.read_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/' 
-                                    'Methods/Methods_Ecolight/Jan2024_lognormal_priors/checks/check_0/check0_x.csv')
+                                    'Methods/Methods_Ecolight/Jan2024_lognormal_priors/'
+                                    'checks_with_noise/check0_1000STR/check0_x.csv')
+print(simulated_reflectance)
+
 # Read the csv file containing the inputs of each of the HydroLight simulation runs
 ecolight_input = pd.read_csv('C:/Users/pirtapalola/Documents/DPhil/Chapter2/'
-                             'Methods/Methods_Ecolight/Jan2024_lognormal_priors/checks/check_0/check0_theta.csv')
+                             'Methods/Methods_Ecolight/Jan2024_lognormal_priors/'
+                             'checks_with_noise/check0_1000STR/check0_theta.csv')
+
+print(ecolight_input)
 
 # Define theta and x.
-theta_example = ecolight_input.iloc[32]  # Theta contains the five input variables
+theta_example = ecolight_input.iloc[10]  # Theta contains the five input variables
 print(theta_example)
 constant = 1.0  # Add a constant to avoid issues with the log-transformation of small values
 theta_example[:3] += constant  # Only add the constant to the first 3 theta parameters
 for x in range(4):  # Apply the log-transformation to the first 4 theta parameters
     theta_example[x] = np.log(theta_example[x])
 
-x_array = simulated_reflectance.iloc[32]  # X contains the simulated spectra
+x_array = simulated_reflectance.iloc[10]  # X contains the simulated spectra
+print(x_array)
 
 # Convert to tensors
 theta_tensor = torch.tensor(theta_example, dtype=torch.float32)
@@ -39,11 +46,11 @@ x_tensor = torch.tensor(x_array, dtype=torch.float32)
 
 # Load the posterior
 with open("C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/"
-          "Jan2024_lognormal_priors/loaded_posterior9.pkl", "rb") as handle:
+          "Jan2024_lognormal_priors/posteriors_saved/loaded_posterior9.pkl", "rb") as handle:
     loaded_posterior = pickle.load(handle)
 
 results_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
-               'Jan2024_lognormal_priors/checks/check_0/'
+               'Jan2024_lognormal_priors/checks_with_noise/check0_1000STR/'
 
 
 def infer_from_simulated_spectra(x_sim, x_sim_parameters):
