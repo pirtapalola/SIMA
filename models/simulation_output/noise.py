4b3af44b5013892ca460b044b2d46fe3da9a154a
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-04-14 14:20:00 +0100

    field and simulated data

--- models/simulation_output/noise.py
+++ models/simulation_output/noise.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 # Read the simulated reflectance data
 path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
-       'Jan2024_lognormal_priors/field_data/field_surface_reflectance_no_noise.csv'
+       'Jan2024_lognormal_priors/simulated_reflectance_no_noise.csv'
 simulated_spectra = pd.read_csv(path)
 
 # Create a list of wavelengths
@@ -30,22 +30,22 @@ num_wavelengths = len(wavelengths)  # 400-700nm at 5nm resolution
 
 # Check what the data looks like before adding noise
 simulated_spectra_original = pd.read_csv(path)
-# no_noise = simulated_spectra_original.iloc[1]
-no_noise = simulated_spectra_original["ONE05"]
+no_noise = simulated_spectra_original.iloc[1]
+# no_noise = simulated_spectra_original["ONE05"]
 print(no_noise)
 
 # Add Gaussian noise
-for i in num_spectra:
-    # spectrum = simulated_spectra.iloc[i]  # Modify one spectrum at a time
-    spectrum = simulated_spectra[i]
+for i in range(len(num_spectra)):
+    spectrum = simulated_spectra.iloc[i]  # Modify one spectrum at a time
+    # spectrum = simulated_spectra[i]
     for wavelength in range(len(wavelengths)):
         std_dev = np.sqrt(np.mean(spectrum ** 2)) / np.sqrt(1000)
         noise = np.random.normal(0, std_dev, 1)  # Generate noise for each wavelength
         spectrum[wavelength] += noise  # Add noise to the current wavelength
 
 # Print an example
-# noise_added = simulated_spectra.iloc[1]
-noise_added = simulated_spectra["ONE05"]
+noise_added = simulated_spectra.iloc[1]
+# noise_added = simulated_spectra["ONE05"]
 print(noise_added)
 
 # Plot an example of a spectrum before and after noise was added
@@ -56,5 +56,5 @@ plt.show()
 
 # Save the results into a csv file
 output_path = 'C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/' \
-              'Jan2024_lognormal_priors/field_data/field_surface_reflectance_1000SNR_noise.csv'
+              'Jan2024_lognormal_priors/simulated_reflectance_1000SNR_noise.csv'
 simulated_spectra.to_csv(output_path)
