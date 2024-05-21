aeadb9eabaad7ef8c24641a9cdbf7999e5f21d37
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-05-21 17:35:18 +0100

    different plot

--- models/posterior_predictive_check2.py
+++ models/posterior_predictive_check2.py
@@ -24,21 +24,27 @@ x_o = torch.tensor([observed])
 # Define your simulated spectra from the posterior predictive distribution x_pp (shape: [1000, 61])
 x_pp = torch.tensor(simulated_reflectance_ppc.values)
 
-# Plot
+# Plot posterior predictives with mean +- std
+# Often +-2std is used, so change factor to 2
 
-# plot posterior/prior samples
-fig, ax = analysis.pairplot(
-    x_pp,
-    # limits=plotting_limits,
-    figsize=(4, 4),
-    points=x_o,
-    # labels=your_labels,
-    points_colors=["red"],  # colors for the points_1 and points_2
-    upper="contour",  # 'scatter' hist, scatter, contour, cond, None
-    **{  #'title': f'Paramter prediction given model {model_sample.numpy()}', # title
-        "kde_offdiag": {"bins": 50},  # bins for kde on the off-diagonal
-        "points_offdiag": {"markersize": 3, "marker": "x"},
-        "contour_offdiag": {"levels": [0.023, 0.5, 0.977]},
-        "points_diag": {"ls": "-", "lw": 1, "alpha": 1},
-    }
-)
+
+def plot_std(x, y, alpha_fill=0.3, factor=1, **kwargs):
+    """plots the mean +-std of y
+
+    Args:
+        x (array): (l))
+        y (array): (n,l)
+        factor (float): factor to multiply std with
+    """
+    mean = np.mean(y, 0)
+    std = np.std(y, 0) * factor
+
+    (base_line,) = plt.plot(x, mean, **kwargs)
+    kwargs["label"] = None
+    kwargs["alpha"] = alpha_fill
+    kwargs["facecolor"] = base_line.get_color()
+    kwargs["edgecolor"] = None  # "green"
+    plt.fill_between(x, mean - std, mean + std, **kwargs)
+
+
+plt.show()
