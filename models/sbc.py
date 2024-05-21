3360245c9898bae66cb4c248704a74648df2dc07
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-05-21 16:00:22 +0100

    test different hyperparameter tuning

--- models/sbc.py
+++ models/sbc.py
@@ -22,8 +22,8 @@ from sbi.analysis.plot import sbc_rank_plot
 """STEP 1. Read the simulated dataset."""
 
 # Read the csv file containing the simulated reflectance data
-simulated_reflectance = pd.read_csv('C:/Users/kell5379/Documents/Chapter2_May2024/downsampled/'
-                                    'simulated_S2_1000SNR_sbc.csv')
+simulated_reflectance = pd.read_csv('C:/Users/kell5379/Documents/Chapter2_May2024/'
+                                    'simulated_reflectance_1000SNR_noise_sbc.csv')
 
 #wavelengths = [443, 490, 531, 565, 610, 665, 700]
 #wavelengths = [str(item) for item in wavelengths]
@@ -88,8 +88,8 @@ print("Shape of X: ", xs.shape)
 """STEP 3. Load the posterior."""
 
 # Load the posterior
-with open("C:/Users/kell5379/Documents/Chapter2_May2024/downsampled/"
-          "loaded_posteriors/loaded_posterior_S2.pkl", "rb") as handle:
+with open("C:/Users/kell5379/Documents/Chapter2_May2024/Noise_1000SNR/Noise_1000SNR/"
+          "loaded_posteriors/loaded_posterior26.pkl", "rb") as handle:
     loaded_posterior = pickle.load(handle)
 
 """STEP 3. Run SBC."""
