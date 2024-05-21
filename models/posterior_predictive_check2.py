d9e173b5c78de09276fe984f8434a961bec5177d
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-05-21 16:01:02 +0100

    new plot

--- models/posterior_predictive_check2.py
+++ models/posterior_predictive_check2.py
@@ -2,6 +2,8 @@ from sbi.analysis import pairplot
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
+import numpy as np
+from sbi import analysis
 
 # Read the csv file containing the simulated reflectance data
 simulated_reflectance = pd.read_csv('C:/Users/kell5379/Documents/Chapter2_May2024/'
@@ -22,17 +24,21 @@ x_o = torch.tensor([observed])
 # Define your simulated spectra from the posterior predictive distribution x_pp (shape: [1000, 61])
 x_pp = torch.tensor(simulated_reflectance_ppc.values)
 
-# Plot the pairplot
-_ = pairplot(
-    samples=x_pp,
-    points=x_o,
-    limits=torch.tensor([[-2.0, 5.0]] * 61),  # Adjust limits based on your data if needed
-    points_colors=["red"],
-    figsize=(8, 8),
-    offdiag="scatter",
-    scatter_offdiag=dict(marker=".", s=5),
-    points_offdiag=dict(marker="+", markersize=20),
-    labels=[rf"$x_{d}$" for d in range(61)],  # Assuming 61 wavelengths
-)
+# Plot
 
-plt.show()
+# plot posterior/prior samples
+fig, ax = analysis.pairplot(
+    x_pp,
+    # limits=plotting_limits,
+    figsize=(4, 4),
+    points=x_o,
+    # labels=your_labels,
+    points_colors=["red"],  # colors for the points_1 and points_2
+    upper="contour",  # 'scatter' hist, scatter, contour, cond, None
+    **{  #'title': f'Paramter prediction given model {model_sample.numpy()}', # title
+        "kde_offdiag": {"bins": 50},  # bins for kde on the off-diagonal
+        "points_offdiag": {"markersize": 3, "marker": "x"},
+        "contour_offdiag": {"levels": [0.023, 0.5, 0.977]},
+        "points_diag": {"ls": "-", "lw": 1, "alpha": 1},
+    }
+)
