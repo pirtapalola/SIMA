53a3f5871457e86a69ed7417b93d3c514eab656e
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-05-22 10:46:35 +0100

    plot to include 5th and 95th percentiles

--- models/posterior_predictive_check2.py
+++ models/posterior_predictive_check2.py
@@ -1,9 +1,7 @@
-from sbi.analysis import pairplot
+# Import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
-import torch
 import numpy as np
-from sbi import analysis
 
 # Read the csv file containing the simulated reflectance data
 simulated_reflectance = pd.read_csv('C:/Users/kell5379/Documents/Chapter2_May2024/'
@@ -16,35 +14,62 @@ observed = simulated_reflectance.iloc[0]
 simulated_reflectance_ppc = pd.read_csv('C:/Users/kell5379/Documents/Chapter2_May2024/PPC/'
                                         'simulated_reflectance_no_noise.csv')
 
-# x_pp = simulated_reflectance.iloc[0]
-
 # Define your observed spectrum x_o (shape: [61])
-x_o = torch.tensor([observed])
+x_o = np.array(observed)
+print("Shape of x_o: ", x_o.shape)
 
 # Define your simulated spectra from the posterior predictive distribution x_pp (shape: [1000, 61])
-x_pp = torch.tensor(simulated_reflectance_ppc.values)
+x_pp = np.array(simulated_reflectance_ppc)
+print("Shape of x_pp: ", x_pp.shape)
 
-# Plot posterior predictives with mean +- std
-# Often +-2std is used, so change factor to 2
+# Create a list of wavelengths
+wavelengths = []
+for wavelength in range(400, 705, 5):
+    wavelengths.append(wavelength)
+wavelengths = np.array(wavelengths)
 
 
-def plot_std(x, y, alpha_fill=0.3, factor=1, **kwargs):
-    """plots the mean +-std of y
+# Plot
+def plot_percentiles(x, y, alpha_fill=0.3, **kwargs):
+    """Plots the mean and 5th to 95th percentile band of y.
 
     Args:
-        x (array): (l))
-        y (array): (n,l)
-        factor (float): factor to multiply std with
+        x (array): Shape (l,)
+        y (array): Shape (n, l)
     """
-    mean = np.mean(y, 0)
-    std = np.std(y, 0) * factor
+    mean = np.mean(y, axis=0)
+    perc_5 = np.percentile(y, 5, axis=0)
+    perc_95 = np.percentile(y, 95, axis=0)
 
     (base_line,) = plt.plot(x, mean, **kwargs)
     kwargs["label"] = None
     kwargs["alpha"] = alpha_fill
     kwargs["facecolor"] = base_line.get_color()
-    kwargs["edgecolor"] = None  # "green"
-    plt.fill_between(x, mean - std, mean + std, **kwargs)
+    kwargs["edgecolor"] = None
+    plt.fill_between(x, perc_5, perc_95, **kwargs)
 
+
+# Mean Squared Error Calculation
+mse_posterior = np.mean((np.mean(x_pp, axis=0) - x_o) ** 2)
+
+# Plotting
+plt.figure(figsize=(10, 5))
+
+# Plot Posterior Predictive
+plot_percentiles(wavelengths, x_pp, alpha_fill=0.3, label=f'Posterior Predictive (MSE={mse_posterior:.2f})',
+                 color='purple')
+
+# Plot Ground Truth (GT)
+plt.plot(wavelengths, x_o, label='Ground Truth (GT)', color='red')
+
+# Setting up the plot appearance
+plt.xlim([wavelengths.min(), wavelengths.max()])
+plt.ylim([x_pp.min() - 0.5, x_pp.max() + 0.5])
+plt.axvline(x=0, linestyle='--', color='brown')
+
+# Labels and legend
+plt.xlabel('Wavelength')
+plt.ylabel('Spectral Value')
+plt.legend()
 
 plt.show()
