8d952b4c6a169cb9f1e1f8d42de364d3ea29749a
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-08-01 13:55:32 +0300

    adjust colors and marker styles

--- visualise/scatter_plot2.py
+++ visualise/scatter_plot2.py
@@ -4,29 +4,35 @@ import pandas as pd
 
 # Read the csv file containing the data into a pandas dataframe
 data_path = "C:/Users/kell5379/Documents/Chapter2_May2024/Final/Results1_constrained/Summary_Hyper/"
-data = pd.read_csv(data_path + "CI_intervals_summary.csv")
+data = pd.read_csv(data_path + "CI_widths_summary.csv")
+
 snr_level_list = []
 for item in data['SNR']:
     snr_level_list.append(str(item))
 data["SNR"] = snr_level_list
 
-plt.figure(figsize=(4, 6))
+plt.figure(figsize=(5, 6))
 colors = {'50': '#a3f7b5', '100': '#7fc8f8', '500': '#08a045'}
+marker_styles = {'50': 'o', '100': 's', '500': 'X'}
+
+# Plot with hue for colors and style for markers
+sns.scatterplot(
+    data=data,
+    x="Parameter",
+    y="CI_value",
+    hue="SNR",
+    style="SNR",
+    palette=colors,
+    markers=marker_styles,
+    legend='full'
+)
+
+"""
 for snr_level in data['SNR'].unique():
     subset = data[data['SNR'] == snr_level]
-    sns.scatterplot(subset, x="Parameter", y='CI_value', color=colors[snr_level], label=snr_level)
-# sns.scatterplot(data=data, x='Parameter', y='CI_value', hue='SNR', style='SNR')
+    sns.scatterplot(subset, x="Parameter", y='CI_value', color=colors[snr_level], label=snr_level)"""
+
 plt.ylabel('Frequency')
+plt.legend(title='SNR')
+plt.savefig(data_path + 'CI_widths.tiff')  # Save the figure as a tiff file
 plt.show()
-
-# sns.pairplot(data, hue='Parameter')  # or hue='Parameter'
-
-# Define distinct colors and opacity levels
-colors = {'Phy': '#a8dadc', 'Min': '#6096ba', 'Wind': '#a3cef1', 'Depth': '#1d3557'}
-alpha = 0.9  # Adjust this value to change opacity
-
-plt.figure(figsize=(10, 6))
-# sns.histplot(data=data, x="CI_value", hue='Parameter', bins=30)
-for param in data['Parameter'].unique():
-    subset = data[data['Parameter'] == param]
-    sns.histplot(subset, x="CI_value", color=colors[param], alpha=alpha, label=param, kde=False, bins=20)
