885723dcd5dbbc78c578c534492640fdadf81e5e
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-08-01 10:11:57 +0300

    apply to multi

--- visualise/scatter_plot.py
+++ visualise/scatter_plot.py
@@ -17,9 +17,9 @@ import numpy as np
 """STEP 1. Prepare the data."""
 
 # Read the csv file containing the data into a pandas dataframe
-results_path = "C:/Users/kell5379/Documents/Chapter2_May2024/Final/Results1_constrained/Summary_Hyper/"
-results_SNR = "1000SNR/"
-results_param = "Phy"
+results_path = "C:/Users/kell5379/Documents/Chapter2_May2024/Final/Results1_constrained/Summary_Multi/"
+results_SNR = "Multi_500SNR/"
+results_param = "Depth"
 results_df = pd.read_csv(results_path + results_SNR + results_param + ".csv")
 true_values = results_df["True_value"]
 means = results_df["Mean"]
@@ -46,7 +46,7 @@ plt.errorbar(x, means, yerr=[lower_errors, upper_errors],
 plt.scatter(x, true_values, color='red', label='True values')
 
 # Define the ticks and labels of the x-axis
-y_ticks = np.arange(0, 8, 1)
+y_ticks = np.arange(0, 25, 5)
 x_ticks = np.arange(0, 4, 1)
 x_labels = [f'Site {x}' for x in x_ticks]
 plt.xticks(x_ticks, labels=x_labels)
@@ -54,10 +54,11 @@ plt.yticks(y_ticks)
 
 # Add labels and title
 plt.xlabel('Sampling sites')
-plt.ylabel('Phytoplankton concentration (mg/$\mathregular{m^3}$)')
-# lt.ylabel('Wind speed (m/s)')
-# plt.ylabel('Depth (m)')
+# plt.ylabel('Phytoplankton concentration (mg/$\mathregular{m^3}$)')
+# plt.ylabel('Wind speed (m/s)')
+plt.ylabel('Depth (m)')
 plt.legend()
 
 # Save the plot
 plt.savefig(results_path + results_SNR + results_param + '.tiff')  # Save the figure as a tiff file
+plt.show()  # Show the plot
