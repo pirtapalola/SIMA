9311ebb0941d23e0d5e15ce24558993447b1f633
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-08-01 11:10:42 +0300

    calculate widths of the 95-percent CIs

--- visualise/scatter_plot.py
+++ visualise/scatter_plot.py
@@ -3,6 +3,7 @@
 Produce a scatterplot to visualize the results of the inference.
 STEP 1. Prepare the data.
 STEP 2. Make the plot.
+STEP 3. Calculate the width of the confidence intervals.
 
 Last updated on 1 August 2024 by Pirta Palola
 
@@ -17,9 +18,9 @@ import numpy as np
 """STEP 1. Prepare the data."""
 
 # Read the csv file containing the data into a pandas dataframe
-results_path = "C:/Users/kell5379/Documents/Chapter2_May2024/Final/Results1_constrained/Summary_Multi/"
-results_SNR = "Multi_500SNR/"
-results_param = "Depth"
+results_path = "C:/Users/kell5379/Documents/Chapter2_May2024/Final/Results1_constrained/Summary_Hyper/"
+results_SNR = "50SNR/"
+results_param = "Phy"
 results_df = pd.read_csv(results_path + results_SNR + results_param + ".csv")
 true_values = results_df["True_value"]
 means = results_df["Mean"]
@@ -60,5 +61,23 @@ plt.ylabel('Depth (m)')
 plt.legend()
 
 # Save the plot
-plt.savefig(results_path + results_SNR + results_param + '.tiff')  # Save the figure as a tiff file
+# plt.savefig(results_path + results_SNR + results_param + '.tiff')  # Save the figure as a tiff file
 plt.show()  # Show the plot
+
+"""STEP 3. Calculate the width of the 95% confidence intervals."""
+
+# Calculate the width of the confidence intervals
+widths = upper_bounds - lower_bounds
+CI_df = pd.DataFrame(widths, columns=[results_param + "_CI_Width"])  # Save into a dataframe
+CI_df.to_csv(results_path + results_SNR + results_param + '_CI_Width.csv', index=False)  # Save as a csv file
+
+# Calculate the distance between the true value and the 95% confidence interval
+distances = []
+for i in x:
+    if true_values[i] < lower_bounds[i]:
+        distance = lower_bounds[i] - true_values[i]
+    elif true_values[i] > upper_bounds[i]:
+        distance = true_values[i] - upper_bounds[i]
+    else:
+        distance = 0
+    distances.append(distance)
