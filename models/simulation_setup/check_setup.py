73dde62abacec40337b760f2e4e9f5c44926d583
pirtapalola <pirta.palola@ouce.ox.ac.uk>
2024-08-27 10:31:30 +0300

    add comment

--- models/simulation_setup/check_setup.py
+++ models/simulation_setup/check_setup.py
@@ -1,10 +1,20 @@
-# Check set-up files
+"""
 
-# reading files
-f1 = open('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-          'Jan2024_lognormal_priors/setup/Icorals_00_032_016_671_799_1261_coralbrown.txt', 'r')
-f2 = open('C:/Users/pirtapalola/Documents/DPhil/Chapter2/Methods/Methods_Ecolight/'
-          'Jan2024_lognormal_priors/setup/Icorals_00_018_207_2198_528_1834_coralbrown.txt', 'r')
+SIMULATION SET-UP I: Checking set-up files
+This code is part of the project "Simulation-based inference for marine remote sensing" by Palola et al.
+
+Check the EcoLight set-up files by identifying differences between two files line-by-line.
+
+The simulated data is available in the associated Open Science Framework data repository:
+Palola, P. (2024, August 26). SBI_marine_remote_sensing. osf.io/pcdgv
+
+Last updated on 27 August 2024
+
+"""
+
+# Open the files
+f1 = open("data/simulated_data/Icorals_00_032_016_671_799_1261_coralbrown.txt", "r")
+f2 = open("data/simulated_data/Icorals_00_018_207_2198_528_1834_coralbrown.txt", "r")
 
 f1_data = f1.readlines()
 f2_data = f2.readlines()
